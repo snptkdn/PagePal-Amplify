740b7b0b4869e10820322772b6ee2c49f0551b99
Ryota KITA <snptkdn@Ryotas-MacBook-Air.local>
2023-05-04 01:03:48 +0900

    menuをコンポーネント化した。

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,7 @@
 <script>
+	import Menu from './menu.svelte';
 </script>
 
-<ion-menu content-id="main-content">
-	<ion-header>
-		<ion-toolbar>
-			<ion-title>Menu</ion-title>
-		</ion-toolbar>
-	</ion-header>
-	<ion-content>
-		<ion-list>
-			<ion-item button> Users </ion-item>
-			<ion-item button> Books </ion-item>
-			<ion-item button> Ranking </ion-item>
-		</ion-list>
-	</ion-content>
-	<ion-footer>
-		<ion-toolbar>
-			<ion-avatar slot="start">
-				<img
-					alt="Silhouette of a person's head"
-					src="https://ionicframework.com/docs/img/demos/avatar.svg"
-				/>
-			</ion-avatar>
-			<ion-label> Test User </ion-label>
-		</ion-toolbar>
-	</ion-footer>
-</ion-menu>
-<div class="ion-page" id="main-content">
-	<ion-header>
-		<ion-toolbar>
-			<ion-buttons slot="start">
-				<ion-menu-button />
-			</ion-buttons>
-			<ion-title>PagePal</ion-title>
-		</ion-toolbar>
-	</ion-header>
-	<ion-content class="ion-padding"> Welcome to PagePal. </ion-content>
-</div>
-
-<style>
-	ion-avatar img {
-		width: 60% !important;
-		height: 60% !important;
-    margin: 15%;
-	}
-</style>
+<Menu>
+	<ion-content class="ion-padding"> hello </ion-content>
+</Menu>
---
+++ src/routes/menu.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	const Navigate = () => {
+		goto('/books');
+	};
+</script>
+
+<ion-menu content-id="main-content">
+	<ion-header>
+		<ion-toolbar>
+			<ion-title>Menu</ion-title>
+		</ion-toolbar>
+	</ion-header>
+	<ion-content>
+		<ion-list>
+			<ion-item button> Users </ion-item>
+			<ion-item button> Books </ion-item>
+			<ion-item button on:click={Navigate}> Ranking </ion-item>
+		</ion-list>
+	</ion-content>
+	<ion-footer>
+		<ion-toolbar>
+			<ion-avatar>
+				<img
+					alt="Silhouette of a person's head"
+					src="https://ionicframework.com/docs/img/demos/avatar.svg"
+				/>
+			</ion-avatar>
+			<ion-label> Test User </ion-label>
+		</ion-toolbar>
+	</ion-footer>
+</ion-menu>
+<div class="ion-page" id="main-content">
+	<ion-header>
+		<ion-toolbar>
+			<ion-buttons slot="start">
+				<ion-menu-button />
+			</ion-buttons>
+			<ion-title>PagePal</ion-title>
+		</ion-toolbar>
+	</ion-header>
+
+  <slot />
+</div>
