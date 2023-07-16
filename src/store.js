7bad69ade230c01d19e08f29b0c53a7eb80a4083
Ryota KITA <snptkdn@Ryotas-MacBook-Air.local>
2023-07-17 01:27:45 +0900

    一旦まーじを取り消す。

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { readable } from 'svelte/store';
+import { readable, writable } from 'svelte/store';
 
 export const BackendHost = readable('http://localhost:3000');
 // export const BackendHost = readable('https://ioujamzf2k.execute-api.ap-northeast-1.amazonaws.com/dev/');
