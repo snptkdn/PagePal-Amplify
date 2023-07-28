3d5ea78babf7e2c839fa712be0311f443a9c04f6
Ryota KITA <snptkdn@Ryotas-MacBook-Air.local>
2023-07-28 23:31:13 +0900

    本履歴にレーティング関係を実装した。

--- src/routes/Components/BookShelf.svelte
+++ src/routes/Components/BookShelf.svelte
@@ -1,5 +1,11 @@
 <script>
 	export let histories;
+
+  const getDateString = (date) => {
+    const datetime = new Date(date);
+    console.log(typeof datetime);
+    return datetime.getFullYear() + "年" + datetime.getMonth() + "月" + datetime.getDate() + "日"
+  }
 </script>
 
 <ion-card>
@@ -16,6 +22,10 @@
 					<h2>{history.Book.title}</h2>
 					<h3>{history.Book.author}</h3>
 					<p>{history.Book.description}</p>
+					<h3>Rating</h3>
+					<p>{history.Rate}</p>
+					<h3>Date</h3>
+					<p>{getDateString(history.Date)}</p>
 				</ion-label>
 			</ion-item>
 		{/each}
--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import IonNav from 'ionic-svelte/components/IonNav.svelte';
 	import Menu from '../menu.svelte';
-  import { BackendHost, CurrentUser } from '../../store.js'
-  import { onMount } from 'svelte';
-  import { goto } from '$app/navigation';
-  import { getCookie } from 'svelte-cookie';
+	import { star } from 'ionicons/icons';
+	import { BackendHost, CurrentUser } from '../../store.js';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { getCookie } from 'svelte-cookie';
 
-  onMount(async () => {
-    const userID = getCookie('userID');
-    if (!userID) {
-      window.location.href = '/';
-    }
-    $CurrentUser.ID = Number(userID);
-  });
+	onMount(async () => {
+		const userID = getCookie('userID');
+		if (!userID) {
+			window.location.href = '/';
+		}
+		$CurrentUser.ID = Number(userID);
+	});
 
 	export let books: Object[] = [];
 	const handleInput = async (event) => {
@@ -27,16 +28,19 @@
 
 	let modal;
 	let selectedBook = null;
+	let rate;
+	let date;
 
-	const postReadHistory = async (book) => {
+	const postReadHistory = async (book, rate, date) => {
+    console.log(date);
 		const isbn = book.volumeInfo.industryIdentifiers.find((id) => (id.type = 'ISBN_13')).identifier;
 		const data = {
 			isbn: isbn,
 			author: book.volumeInfo.authors ? book.volumeInfo.authors[0] : null,
 			page_count: book.volumeInfo.pageCount,
 			title: book.volumeInfo.title,
-      image_url: book.volumeInfo.imageLinks?.smallThumbnail,
-      description: book.volumeInfo.description,
+			image_url: book.volumeInfo.imageLinks?.smallThumbnail,
+			description: book.volumeInfo.description
 		};
 		await fetch(`${$BackendHost}/books`, {
 			method: 'POST',
@@ -54,11 +58,21 @@
 			.then((json) => json);
 
 		console.log(target_book.ID);
+    
+    let today = new Date();
+    today.setHours(9);
+    if (date === undefined) {
+      date = today.toISOString();
+    } else {
+      date = date+"Z";
+    }
 
 		const history_data = {
 			user_id: $CurrentUser.ID,
 			book_id: target_book.ID,
-      is_read: true
+			is_read: true,
+      rate: rate,
+      date: date,
 		};
 
 		await fetch(`${$BackendHost}/read_histories`, {
@@ -82,110 +96,123 @@
 	};
 </script>
 
-	<ion-searchbar placeholder="book name" on:ionChange={handleInput} />
+<ion-searchbar placeholder="book name" on:ionChange={handleInput} />
+<ion-content class="ion-padding">
+	{#if books.length > 0}
+		{#each books as book}
+			{#if book.volumeInfo.industryIdentifiers.some((id) => id.type == 'ISBN_13')}
+				<ion-card>
+					{#if 'imageLinks' in book.volumeInfo}
+						<img alt="Silhouette of mountains" src={book.volumeInfo.imageLinks.smallThumbnail} />
+					{:else}
+						<img
+							alt="Silhouette of mountains"
+							src="https://ionicframework.com/docs/img/demos/card-media.png"
+						/>
+					{/if}
+					<ion-card-header>
+						<ion-card-title>{book.volumeInfo.title}</ion-card-title>
+						{#if 'authors' in book.volumeInfo}
+							<ion-card-subtitle>著者: {book.volumeInfo.authors[0]}</ion-card-subtitle>
+						{/if}
+						{#if 'publishedDate' in book.volumeInfo}
+							<ion-card-subtitle>発売日: {book.volumeInfo.publishedDate}</ion-card-subtitle>
+						{/if}
+					</ion-card-header>
+
+					<ion-button expand="block" on:click={() => openModal(book)} color="medium">
+						<ion-label>View Detail</ion-label>
+					</ion-button>
+				</ion-card>
+			{/if}
+		{/each}
+	{:else}
+		<ion-content>There are no books.</ion-content>
+	{/if}
+
 	<ion-content class="ion-padding">
-		{#if books.length > 0}
-			{#each books as book}
-				{#if book.volumeInfo.industryIdentifiers.some((id) => id.type == 'ISBN_13')}
-					<ion-card>
-						{#if 'imageLinks' in book.volumeInfo}
-							<img alt="Silhouette of mountains" src={book.volumeInfo.imageLinks.smallThumbnail} />
+		<ion-modal bind:this={modal}>
+			{#if selectedBook !== null}
+				<ion-header>
+					<ion-toolbar>
+						<ion-buttons slot="secondary">
+							<ion-button
+								expand="block"
+								on:click={() => postReadHistory(selectedBook, rate.value, date.value)}
+								color="warning"
+							>
+								<ion-label>読んだ！</ion-label>
+							</ion-button>
+						</ion-buttons>
+						<ion-title>{selectedBook.volumeInfo.title}</ion-title>
+						<ion-buttons slot="primary">
+							<ion-button on:click={closeModal}>Close</ion-button>
+						</ion-buttons>
+					</ion-toolbar>
+				</ion-header>
+				<ion-content class="ion-padding">
+					{#if 'imageLinks' in selectedBook.volumeInfo}
+						<img
+							alt="Silhouette of mountains"
+							src={selectedBook.volumeInfo.imageLinks.smallThumbnail}
+						/>
+					{:else}
+						<img
+							alt="Silhouette of mountains"
+							src="https://ionicframework.com/docs/img/demos/card-media.png"
+						/>
+					{/if}
+					<ion-item-group>
+						<ion-item-divider>
+							<ion-label> AUTHOR </ion-label>
+						</ion-item-divider>
+						{#if 'authors' in selectedBook.volumeInfo}
+							<ion-item>{selectedBook.volumeInfo.authors[0]}</ion-item>
 						{:else}
-							<img
-								alt="Silhouette of mountains"
-								src="https://ionicframework.com/docs/img/demos/card-media.png"
-							/>
+							<ion-item> No Info </ion-item>
 						{/if}
-						<ion-card-header>
-							<ion-card-title>{book.volumeInfo.title}</ion-card-title>
-							{#if 'authors' in book.volumeInfo}
-								<ion-card-subtitle>著者: {book.volumeInfo.authors[0]}</ion-card-subtitle>
-							{/if}
-							{#if 'publishedDate' in book.volumeInfo}
-								<ion-card-subtitle>発売日: {book.volumeInfo.publishedDate}</ion-card-subtitle>
-							{/if}
-						</ion-card-header>
 
-						<ion-button expand="block" on:click={() => openModal(book)} color="medium">
-							<ion-label>View Detail</ion-label>
-						</ion-button>
-					</ion-card>
-				{/if}
-			{/each}
-		{:else}
-			<ion-content>There are no books.</ion-content>
-		{/if}
-
-		<ion-content class="ion-padding">
-			<ion-modal bind:this={modal}>
-				{#if selectedBook !== null}
-					<ion-header>
-						<ion-toolbar>
-							<ion-buttons slot="secondary">
-								<ion-button
-									expand="block"
-									on:click={() => postReadHistory(selectedBook)}
-									color="warning"
-								>
-									<ion-label>読んだ！</ion-label>
-								</ion-button>
-							</ion-buttons>
-							<ion-title>{selectedBook.volumeInfo.title}</ion-title>
-							<ion-buttons slot="primary">
-								<ion-button on:click={closeModal}>Close</ion-button>
-							</ion-buttons>
-						</ion-toolbar>
-					</ion-header>
-					<ion-content class="ion-padding">
-						{#if 'imageLinks' in selectedBook.volumeInfo}
-							<img
-								alt="Silhouette of mountains"
-								src={selectedBook.volumeInfo.imageLinks.smallThumbnail}
-							/>
+						<ion-item-divider>
+							<ion-label> PUBLISH DATE </ion-label>
+						</ion-item-divider>
+						{#if 'publishedDate' in selectedBook.volumeInfo}
+							<ion-item>{selectedBook.volumeInfo.publishedDate}</ion-item>
 						{:else}
-							<img
-								alt="Silhouette of mountains"
-								src="https://ionicframework.com/docs/img/demos/card-media.png"
-							/>
+							<ion-item> No Info </ion-item>
 						{/if}
-						<ion-item-group>
-							<ion-item-divider>
-								<ion-label> AUTHOR </ion-label>
-							</ion-item-divider>
-							{#if 'authors' in selectedBook.volumeInfo}
-								<ion-item>{selectedBook.volumeInfo.authors[0]}</ion-item>
-							{:else}
-								<ion-item> No Info </ion-item>
-							{/if}
 
-							<ion-item-divider>
-								<ion-label> PUBLISH DATE </ion-label>
-							</ion-item-divider>
-							{#if 'publishedDate' in selectedBook.volumeInfo}
-								<ion-item>{selectedBook.volumeInfo.publishedDate}</ion-item>
-							{:else}
-								<ion-item> No Info </ion-item>
-							{/if}
+						<ion-item-divider>
+							<ion-label> PAGE COUNT </ion-label>
+						</ion-item-divider>
+						{#if 'pageCount' in selectedBook.volumeInfo}
+							<ion-item>{selectedBook.volumeInfo.pageCount}</ion-item>
+						{:else}
+							<ion-item> No Info </ion-item>
+						{/if}
 
-							<ion-item-divider>
-								<ion-label> PAGE COUNT </ion-label>
-							</ion-item-divider>
-							{#if 'pageCount' in selectedBook.volumeInfo}
-								<ion-item>{selectedBook.volumeInfo.pageCount}</ion-item>
-							{:else}
-								<ion-item> No Info </ion-item>
-							{/if}
+						<ion-item-divider>
+							<ion-label> RATING </ion-label>
+							<ion-range pin bind:this={rate}>
+								<ion-icon slot="start" name="snow-outline" />
+								<ion-icon slot="end" name="sunny-outline" />
+							</ion-range>
+						</ion-item-divider>
 
-							<br />
-						</ion-item-group>
+						<ion-item-divider>
+							<ion-label> DATE </ion-label>
+						</ion-item-divider>
+						<ion-datetime presentation="month-year" bind:this={date} />
 
-						<ion-item class="description">{selectedBook.volumeInfo.description}</ion-item>
 						<br />
-					</ion-content>
-				{/if}
-			</ion-modal>
-		</ion-content>
+					</ion-item-group>
+
+					<ion-item class="description">{selectedBook.volumeInfo.description}</ion-item>
+					<br />
+				</ion-content>
+			{/if}
+		</ion-modal>
 	</ion-content>
+</ion-content>
 
 <style>
 	ion-card img {
